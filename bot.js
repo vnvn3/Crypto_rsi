96d566d112928b960661a18b53361c5870342d00
vnvn3 <vnvn3@YAHOO.COM>
2026-09-16 18:43:49 +0330

    Update bot.js

--- bot.js
+++ bot.js
@@ -111,7 +111,7 @@ export async function runBot(env, BOT_TOKEN, CHAT_ID) {
         rsi.at(-1) < rsi.at(-3) &&
         rsi.at(-1) > 65 &&
         volumes.at(-1) > avg(volumes.slice(-20))) {
-      return { name: "دایورجنس منفی", score: 3 };
+      return { name: "🟢دایورجنس منفی", score: 3 };
     }
 
     return null;
@@ -122,12 +122,12 @@ export async function runBot(env, BOT_TOKEN, CHAT_ID) {
 
     if (highs[i] > highs[i - 1] && highs[i] > highs[i + 1] &&
         closes.at(-1) > highs[i]) {
-      return { name: "BOS صعودی", score: 2 };
+      return { name: "🟢BOS صعودی", score: 2 };
     }
 
     if (lows[i] < lows[i - 1] && lows[i] < lows[i + 1] &&
         closes.at(-1) < lows[i]) {
-      return { name: "BOS نزولی", score: 2 };
+      return { name: "🔴BOS نزولی", score: 2 };
     }
 
     return null;
@@ -139,11 +139,11 @@ export async function runBot(env, BOT_TOKEN, CHAT_ID) {
     const i = closes.length - 3;
 
     if (trendDown && closes.at(-1) > highs[i]) {
-      return { name: "CHoCH صعودی", score: 2 };
+      return { name: "🟢CHoCH صعودی", score: 2 };
     }
 
     if (trendUp && closes.at(-1) < lows[i]) {
-      return { name: "CHoCH نزولی", score: 2 };
+      return { name: "🔴CHoCH نزولی", score: 2 };
     }
 
     return null;
